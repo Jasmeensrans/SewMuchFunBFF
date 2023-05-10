6899efedfee35691a166d6f2bc63ffdd81194eb4
Jasmeen Sran <jasmeen.sran@mail.utoronto.ca>
2023-05-09 21:13:40 -0400

    created posts table

--- starter.py
+++ starter.py
@@ -1,6 +1,32 @@
 import boto3;
 
-# some starter code to connect to s3
-s3 = boto3.resource('s3');
-for bucket in s3.buckets.all():
-    print(bucket.name)
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+# note that you do not need to define non-key attributes of table, here we have only defined post_id
+table = dynamodb.create_table(
+    TableName='posts',
+    KeySchema=[
+        {
+            'AttributeName': 'post_id',
+            'KeyType': 'HASH'
+        },
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'post_id',
+            'AttributeType': 'N'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    }
+)
+
+# Wait until the table exists.
+table.wait_until_exists()
+
+# Print out some data about the table.
+print(table.item_count)
